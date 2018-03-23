f7bfd0e2380db43b12f582bd57a6d728e1d54a04
Lora Woodford <lorawoodford@gmail.com>
2018-03-23 10:14:18 -0400

    udpate db migration

--- common/db/migrations/098_remove_bulk_inclusive_events.rb
+++ common/db/migrations/098_remove_bulk_inclusive_events.rb
@@ -3,15 +3,15 @@ require_relative 'utils'
 Sequel.migration do
 
   up do
-    enum = self[:enumeration].filter(:name => 'date_type').select(:id)
-    range = self[:enumeration_value].filter(:value => 'range').select(:id)
+    enum = self[:enumeration].filter(:name => 'date_type').get(:id)
+    range = self[:enumeration_value].filter(:value => 'range').get(:id)
 
     # Changing all event dates that are bulk dates to ranges
-    bulk_date = self[:enumeration_value].filter(:value => "bulk").select(:id)
+    bulk_date = self[:enumeration_value].filter(:value => "bulk").get(:id)
     self[:date].exclude(:event_id => nil).filter(:date_type_id => bulk_date).update(:date_type_id => range)
 
     # Changing all event dates that are inclusive dates to ranges
-    inclusive_date = self[:enumeration_value].filter(:value => "inclusive").select(:id)
+    inclusive_date = self[:enumeration_value].filter(:value => "inclusive").get(:id)
     self[:date].exclude(:event_id => nil).filter(:date_type_id => inclusive_date).update(:date_type_id => range)
 
   end
